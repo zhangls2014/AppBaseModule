6b98080e603d23db94fa53bb417620c354d3621c
zhangls <zhangls2014@gmail.com>
2023-12-12 10:24:51 +0800

    新增 TipsEvent 扩展方法

--- base/src/main/kotlin/com/zhangls/base/data/TipsEvent.kt
+++ base/src/main/kotlin/com/zhangls/base/data/TipsEvent.kt
@@ -12,10 +12,18 @@ sealed class TipsEvent {
         val title: String,
         val content: String,
         val positiveText: String,
-        val negativeText: String?
+        val negativeText: String? = null,
+        // 用于标注 Dialog 事件的类型
+        val action: Int = 0
     ) : TipsEvent()
 
     data class SnackBar(val text: String, val shortTime: Boolean = true) : TipsEvent()
     data class Toast(val text: String, val shortTime: Boolean = true) : TipsEvent()
     data class Loading(val display: Boolean, val text: String = "加载中") : TipsEvent()
 }
+
+fun String.snack(shortTime: Boolean) = TipsEvent.SnackBar(this, shortTime)
+
+fun String.toast(shortTime: Boolean) = TipsEvent.Toast(this, shortTime)
+
+fun String.loading(display: Boolean) = TipsEvent.Loading(display, this)
